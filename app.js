451f2704239aa9fe064ad695789227af58d7c424
bigpreshy <lukeprecious@gmail.com>
2018-07-22 18:47:32 +0100

    first comment

---
+++ app.js
@@ -0,0 +1,87 @@
+window.fn = {};
+
+window.fn.open = function() {
+  var menu = document.getElementById('menu');
+  menu.open();
+};
+
+window.fn.load = function(page) {
+  var content = document.getElementById('content');
+  var menu = document.getElementById('menu');
+  content.load(page)
+    .then(menu.close.bind(menu));
+};
+
+document.addEventListener('init', function(event){
+ if(event.target.id == "home") {
+ 	openDb();
+ 	getItems();
+ }
+
+
+});
+
+ 
+var db = null;
+
+function onSuccess (tx , r){
+getItems();
+}
+function onError( tx, e){
+	alert ("Something is not right");
+}
+function openDb(){
+	db = openDatabase("Mytask", "1", "my task", 2*1024*1024);
+
+
+	db.transaction(function(tx){
+	tx.executeSql("CREATE TABLE IF NOT EXISTS items (ID INTEGER PRIMARY KEY ASC, item TEXT)",[]);
+});
+}
+
+function getItems(){
+ db.transaction(function(tx){
+ 	tx.executeSql("SELECT * FROM items", [], renderItems, onError);
+ });
+
+}
+
+function renderItems (tx, rs)
+{
+ var output = "";
+ var list = document.getElementById("Ajuma");
+
+ for (i = 0; i < rs.rows.length; i++) {
+ 
+
+ var row = rs.rows.item(i);
+ output += "<ons-list-item>" + row.item +
+ "<div class =\"right\"> <ons-button onclick = 'deleteItem(" + row.ID + ");')> <ons-icon icon = \"trash\"></ons-icon> </ons-button></div>" +
+ "</ons-list-item>";
+
+ }
+ list.innerHTML = output;
+}
+
+function Grace(){
+	var textbox = document.getElementById("item");
+	var value = textbox.value;
+
+	db.transaction(function(tx){
+		tx.executeSql("INSERT INTO items (item) VALUES (?)", [value], onSuccess, onError);
+	});
+
+	textbox.value = "";
+	fn.load('home.html');
+}
+
+function deleteItem(id)
+{
+
+db.transaction(function(tx){
+tx.executeSql("DELETE FROM items WHERE ID=?", [id], onSuccess, onError);	
+});
+
+}
+
+
